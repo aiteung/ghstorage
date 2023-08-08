76be0ad9e41a1f5b1873ef3f713b15a52c120b9f
Valen Rionald <vrionald@gmail.com>
2023-08-08 10:25:57 +0700

    Add License

--- upload_test.go
+++ upload_test.go
@@ -7,13 +7,14 @@ import (
 
 func TestUpload(t *testing.T) {
 	// Ganti dengan informasi Anda
+	owner := "valenrio66"
 	org := "repoulbi"
 	repo := "data_simpelbi"
 	branch := "main"
-	filePath := "coba.txt" // Ganti dengan path file yang ingin Anda unggah
-	accessToken := "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"
+	filePath := "presensi.txt" // Ganti dengan path file yang ingin Anda unggah
+	accessToken := "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"
 
-	err := UploadFileToGitHub("valenrio66", org, repo, branch, filePath, accessToken)
+	err := UploadFileToGitHub(owner, org, repo, branch, filePath, accessToken)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
